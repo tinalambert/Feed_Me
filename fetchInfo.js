62c5211fe66c80d6476a7c6473ebbaf0be5f7cdc
Chandler Higley <chandlerhigley@chandlers-air.lan>
2022-02-07 19:37:37 -0700

    First commit

--- fetchInfo.js
+++ fetchInfo.js
@@ -18,42 +18,48 @@ fetch("https://api.nal.usda.gov/fdc/v1/foods/search?api_key=jpBJwOYeophbBI9AwtYh
       'Content-Type': 'application/json',
       // 'Allow': 'GET, POST, HEAD',
    },
-   
-})
-.then(response => {
-   console.log('resolved', response)
-	return response.json()
-})
-.catch(error => {
-   console.log("rejected", error)
-})
-.then(data => {
-   console.log(data);
-   const foods = data.foods;
-   console.log(foods)
 
-   // -- GET FOOOD ITEM ARRAY FROM FETCH DATA --
-   // How to get the full array from all pages?
-   foods.forEach(ingredient => {
-      //console.log(ingredient.description)
-      let searchItem = `<option value="${ingredient.description}">`;
-      foodList.innerHTML += searchItem
+})
+   .then(response => {
+      console.log('resolved', response)
+      return response.json()
    })
-});
+   .catch(error => {
+      console.log("rejected", error)
+   })
+   .then(data => {
+      console.log(data);
+      const foods = data.foods;
+      console.log(foods)
+
+      // -- GET FOOOD ITEM ARRAY FROM FETCH DATA --
+      // How to get the full array from all pages?
+      foods.forEach(ingredient => {
+         //console.log(ingredient.description)
+         let searchItem = `<option value="${ingredient.description}">`;
+         foodList.innerHTML += searchItem
+      })
+   });
 
 // ADD SELECTED ITEM TO MY LIST
 function addItem() {
    // Should add newItem to ingredientList with the correct CSS presentation
-   
+
    let selectedItem = foodSearchInput.value; // value of the searched ingredient
    let newItem = document.createElement('li');
+   if (Element.length === 0) {
+      alert("You must add a valid input");
+   }
    newItem.textContent = selectedItem;
    newItem.className = "list-group-item"
    ingredientList.appendChild(newItem);
    foodSearchInput.value = "";
 
+
+
+
    // ONLY ADD THE ITEM IF IT'S PART OF THE DATA LIST
-   
+
    // if (selectedItem.value.tagName == "DATALIST") {
    //    ingredientList.appendChild(newItem);
    // } else {
@@ -62,10 +68,10 @@ function addItem() {
 
    // }
    //console.log(newItem)
-   
 
 
-// REMOVE LIST ITEMS BUTTON
+
+   // REMOVE LIST ITEMS BUTTON
 
    // foodSearchDIV.addEventListener("click", (e) => {
    //    if (e.target.tagName == "BUTTON") {
